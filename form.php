df940e1ab9bc9d2155eecd4e35e31d1124b4bb0b
Mamba-dot <shahnewaz886@gmail.com>
2021-07-18 21:18:38 +0600

    new

---
+++ form.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        This is <?php echo $_POST["name"]; ?> website.
+    </p>
+    <p>
+        His email is <?php echo $_POST["email"]; ?>.
+    </p>
+    <p>
+        His mobile number is <?php echo $_POST["mobile_no"]; ?>.
+    </p>
+    <p>
+        His password is <?php echo $_POST["password"]; ?>.
+    </p>
+
+</body>
+</html>
